8d91fb2fa4a5e01761589a6376939725648df64b
Bernard Lawes <bernardlawes@gmail.com>
2025-02-13 16:41:13 -0500

    reorgnized file structure

--- Fire/fire.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-path_to_input_image = '.\\_input\\fire.jpg'
-
-path_to_pretrained_model =  '.\\Fire\\fire.pt'
-
-model = YOLO(path_to_pretrained_model)
-model.predict(source=path_to_input_image, imgsz=640, conf=0.6,save=True, project="_output")
---
+++ fire.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+path_to_input_image = '.\\data\\images\\fire.jpg'
+
+path_to_predictions = ".\\output\\"
+
+path_to_model =  '.\\models\\fire.pt'
+
+model = YOLO(path_to_model)
+#model.predict(source=path_to_input_image, imgsz=640, conf=0.6,save=True, project=path_to_predictions)
+model.predict(source=path_to_input_image, imgsz=640, conf=0.6,show=True)
